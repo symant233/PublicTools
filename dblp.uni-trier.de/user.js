253105100c624792f36c216143e42ebea3acdba1
symant233 <symant233@gmail.com>
2022-01-26 11:26:11 +0800

    [dblp] 修复跳转scihub

--- dblp.uni-trier.de/user.js
+++ dblp.uni-trier.de/user.js
@@ -6,7 +6,7 @@
 // @match       https://dblp.org/*
 // @grant       GM_addStyle
 // @run-at      document-end
-// @version     2.0.7
+// @version     2.0.8
 // @author      symant233
 // @description 学术会议、学术期刊 CCF等级标注
 // @homepageURL https://github.com/symant233/PublicTools
@@ -672,7 +672,7 @@
                 }
                 // 根据DOI一键跳转到sci-hub
                 try{
-                    let doi = n.childNodes[2].childNodes[0].childNodes[0].childNodes[0].childNodes[0]
+                    let doi = n.getElementsByTagName('a')[0]
                     doi.href = 'https://sci-hub.ru/' + doi.href
                 }catch(e){}
             }
